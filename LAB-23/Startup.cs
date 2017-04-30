524cebc11325e70600218d7c74dc6fe27b33c1ff
Chuck Knebl <chuck.knebl@att.net>
2017-04-30 18:43:33 -0400

    Commit for Lab 23

---
+++ LAB-23/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LAB_23.Startup))]
+namespace LAB_23
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
